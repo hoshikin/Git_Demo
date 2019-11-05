c5b71471fcaec11d68a12d9fc39725d4d07c716d
purush <purush.narayana@gmail.com>
2019-11-05 15:31:43 +0530

    Commit to Develop Branch

--- src/alpha/Demo.java
+++ src/alpha/Demo.java
@@ -1,6 +1,7 @@
 package alpha;
 
 import org.apache.logging.log4j.*;
+import org.testng.annotations.Test;
 
 public class Demo {
 	
@@ -17,6 +18,21 @@ public class Demo {
 		System.out.println("Test1 Git");
 		
 		
+	}
+	
+	@Test
+	public void gitDemo() {
+		System.out.println("Test1 Git");
+	}
+	
+	@Test
+	public void gitDemo1() {
+		System.out.println("Test2 Git");
+	}
+	
+	@Test
+	public void gitDemo2() {
+		System.out.println("Test3 Git");
 	}
 
 }
