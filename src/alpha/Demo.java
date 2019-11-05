79ba4b73c92eeb6890e78687f42dda640718a5cc
purush <purush.narayana@gmail.com>
2019-11-05 16:17:33 +0530

    test

--- src/alpha/Demo.java
+++ src/alpha/Demo.java
@@ -1,6 +1,7 @@
 package alpha;
 
 import org.apache.logging.log4j.*;
+import org.testng.annotations.Test;
 
 public class Demo {
 	
@@ -13,10 +14,21 @@ public class Demo {
 		log.error("Button is not Displayed");
 		log.fatal("Button is Missing");
 		
-		
+	}
+	
+	@Test
+	public void gitDemo() {
 		System.out.println("Test1 Git");
-		
-		
+	}
+	
+	@Test
+	public void gitDemo1() {
+		System.out.println("Test2 Git");
+	}
+	
+	@Test
+	public void gitDemo2() {
+		System.out.println("Test3 Git");
 	}
 
 }
